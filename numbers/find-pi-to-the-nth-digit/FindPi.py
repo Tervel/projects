29d54e92a7c3980698d3dcbedaeac2bfd31f72c2
Jordan Anslow <jordananslow@gmail.com>
2015-07-22 00:01:28 +1000

    Updated to conform to standards

--- numbers/find-pi-to-the-nth-digit/FindPi.py
+++ numbers/find-pi-to-the-nth-digit/FindPi.py
@@ -1,31 +1,39 @@
-# Spigot algorithm for finding the nth digit of pi
-# based off Unbounded Spigot Algorithms for the Digits of Pi
-# by Jeremy Gibbons[0] and Digits of Pi by John Zelle[1]
-# 
-# [0] www.cs.ox.ac.uk/people/jeremy.gibbons/publications/spigot.pdf
-# [1] mail.python.org/pipermail/edu-sig/2006-July/006810.html 
+"""
+Spigot algorithm for finding the nth digit of pi
+based off Unbounded Spigot Algorithms for the Digits of Pi
+by Jeremy Gibbons[0] and Digits of Pi by John Zelle[1]
 
-def findPi(length):
+[0] www.cs.ox.ac.uk/people/jeremy.gibbons/publications/spigot.pdf
+[1] mail.python.org/pipermail/edu-sig/2006-July/006810.html
+"""
+
+def find_pi(length):
+    """
+    Spitgot algorithm to find pi
+    """
     q, r, t, k, n, l = 1, 0, 1, 1, 3, 3
-    piArray = []
+    pi_array = []
 
     while True:
         if 4*q+r-t < n*t:
-            piArray.append(str(n)) # alternative to yield
+            pi_array.append(str(n)) # alternative to yield
             q, r, t, k, n, l = (10*q, 10*(r-n*t), t, k, (10*(3*q+r))//t-10*n, l)
         else:
             q, r, t, k, n, l = (q*k, (2*q+r)*l, t*l, k+1, (q*(7*k+2)+r*l)//(t*l), l+2)
 
-        if len(piArray) == length:
-            return piArray
+        if len(pi_array) == length:
+            return pi_array
 
 def main():
+    """
+    Get user input and append find_pi output onto output string
+    """
     print("Enter number of digits of pi: ")
     length = input(" >>  ")
 
-    piArray = findPi(int(length))
-    piArray = piArray[:1] + ['.'] + piArray[1:]
-    print ("".join(piArray))
+    pi_array = find_pi(int(length))
+    pi_array = pi_array[:1] + ['.'] + pi_array[1:]
+    print("".join(pi_array))
 
     return
 
