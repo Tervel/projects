d98df836211316c016516463848b2ad59eaf5831
n2 <n2@localhost.localdomain>
2015-07-28 18:29:36 +1000

    Fibonacci sequence, recursive and iterative

---
+++ numbers/fibonacci-sequence/FibonacciSequence.py
@@ -0,0 +1,44 @@
+"""
+Finds the fibonacci sequence to the nth number
+"""
+# def recursive_fibonacci(n):
+#     """
+#     finds the nth fibonacci number recursively
+#     """
+#     if n < 2:
+#         return n
+#     else:
+#         return recursive_fibonacci(n - 1) + recursive_fibonacci(n - 2)
+
+def iterative_fibonacci(n):
+    """
+    iterative algorithm
+    n = n-1 + n-2
+    """
+    i = 1
+    j = 0
+
+    for k in range(1, n + 1):
+        temp = i + j
+        i = j
+        j = temp
+
+    return j
+
+def main():
+    """
+    main function
+    """
+    print "Enter the nth digit of the fibonacci sequence to find: "
+    length = raw_input(" >>  ")
+
+    print iterative_fibonacci(int(length))
+
+    return
+
+
+if __name__ == "__main__":
+    main()
+
+
+
